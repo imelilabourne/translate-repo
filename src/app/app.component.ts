80e35f323a8832c81ea848b7fea00a461ee824ed
imelilabourne <elilabourne.detorres@gmail.com>
2021-01-28 12:14:40 +0800

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,34 @@
-import {Component} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
     <div>
-      <h2>{{ 'HOME.TITLE' | translate }}</h2>
+      <h2>{{ "HOME.TITLE" | translate }}</h2>
+      <p>
+        {{ "PROFILE.DESCRIPTION" | translate }}
+      </p>
       <label>
-        {{ 'HOME.SELECT' | translate }}
+        {{ "HOME.SELECT" | translate }}
         <select #langSelect (change)="translate.use(langSelect.value)">
-          <option *ngFor="let lang of translate.getLangs()" [value]="lang" [selected]="lang === translate.currentLang">{{ lang }}</option>
+          <option
+            *ngFor="let lang of translate.getLangs()"
+            [value]="lang"
+            [selected]="lang === translate.currentLang"
+            >{{ lang }}</option
+          >
         </select>
       </label>
     </div>
-  `,
+  `
 })
 export class AppComponent {
   constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'fr']);
-    translate.setDefaultLang('en');
+    translate.addLangs(["en", "fr", "ph"]);
+    translate.setDefaultLang("en");
 
     const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+    translate.use(browserLang.match(/en|fr/) ? browserLang : "en");
   }
 }
